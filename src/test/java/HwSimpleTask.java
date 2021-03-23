530a2a964c99f1673243e110cb7995d2af3da656
Anna Pogrebinskaya <aniapog@mail.ru>
2021-03-23 17:39:53 +0200

    Select element test

--- src/test/java/HwSimpleTask.java
+++ src/test/java/HwSimpleTask.java
@@ -1,11 +1,14 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import javax.xml.bind.Element;
 import java.util.concurrent.TimeUnit;
 
 public class HwSimpleTask {
@@ -21,6 +24,9 @@ public class HwSimpleTask {
 
     @Test
     public void fillForm(){
+        JavascriptExecutor js = (JavascriptExecutor) wd;
+        js.executeScript("document.querySelector('footer').style.display='none';");
+
         WebElement fullName = wd.findElement(By.id("userName"));
         WebElement email = wd.findElement(By.id("userEmail"));
         WebElement currentAddress = wd.findElement(By.id("currentAddress"));
@@ -40,6 +46,28 @@ public class HwSimpleTask {
         fillField(permanentAddress, text);
 
         button.click();
+
+
+        WebElement getName = wd.findElement(By.id("name"));
+        SelectElementTest(fullName, getName);
+
+        WebElement getEmail = wd.findElement(By.id("email"));
+        SelectElementTest(email, getEmail);
+
+        WebElement getCurrentAddress = wd.findElement(By.id("currentAddress"));
+        SelectElementTest(currentAddress, getCurrentAddress);
+
+        WebElement getPermanentAddress = wd.findElement(By.id("permanentAddress"));
+        SelectElementTest(permanentAddress, getPermanentAddress);
+    }
+
+    @Test
+
+    public void SelectElementTest(WebElement el1, WebElement el2){
+        String text = el2.getText();
+        System.out.println(text);
+
+        Assert.assertTrue(text.contains(el1.getText()));
     }
 
     public void fillField(WebElement el, String text){
